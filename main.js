2b3d5aa34fd65f98eaf7eda253a242b470b84d4c
Baltasar Bravo <brigidobalta@gmail.com>
2023-03-08 23:30:37 -0300

    Agregado de filtro y correciones en bucles

--- main.js
+++ main.js
@@ -8,78 +8,10 @@ const comidaProteica = ["Mantequilla de Mani", "Pavo", "Yogurt griego", "Langost
 
     
 
-//  function opciones() {
-//       let entrada = parseFloat(prompt("Ingrese una opcion")); 
-    
-//       if(entrada === 1){
-//  
-// for(let dias = 0; dias < semana.length; dias++)
-// alert(semana[dias]);
-//     };
-// };
-
-// const dietaHierro = []
-const dietas = [];
-
-// function crearObjeto(semana,comidaHierro){
-
-//     for(let i = 0; i < semana.length; i++){
-//         objeto[semana[i]] = comidaHierro[i];
-        
-//     }
-    
-//     return objeto
-    
-// }
-
-// crearObjeto(["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"], ["Legumbres secas", "Cereales fortificados", "Higado", "Carne de res", "Atun","Frutas deshidratadas", "Granos enteros"]);
-
-// dietaHierro.push(objeto);
-
-// dietaHierro.forEach(dieta => {
-//     let entrada = prompt("Ingrese el dia para conocer que debe comer");
-//     while (entrada != "ESC") {
-//       switch (entrada) {
-//         case "lunes":
-//             alert(dieta.Lunes);
-//             break;
-
-//             case "martes":
-//             alert(dieta.Martes);
-//             break;
-
-//             case "miercoles":
-//             alert(dieta.Miercoles);
-//             break;
-
-//             case "jueves":
-//             alert(dieta.Jueves);
-//             break;
-
-//             case "viernes":
-//             alert(dieta.Viernes);
-//             break;
-
-//             case "sabado":
-//             alert(dieta.Sabado);
-//             break;
-
-//             case "domingo":
-//             alert(dieta.Domingo);
-//             break;
-      
-//         default:
-//             break;
-//       }
-//       entrada = prompt("Ingrese el dia para conocer que debe comer");  
-//     }
-
-// });
+const dietas = {};
 
 
-
-
-class Dieta{
+class Fusion{
     constructor(dia,comidas){
  this.dia = dia;
  this.comidas = comidas;
@@ -91,7 +23,7 @@ class Dieta{
                      dietas[semana[i]] = comidaEnergetica[i];
                  }
                 
-                 return dietas
+                  return dietas
     }
 
     asignarHierro(){
@@ -112,63 +44,160 @@ class Dieta{
 }
 
 
-const entrada = parseFloat(prompt("Ingrese una opción"))
+function opciones(){
+let entrada = parseFloat(prompt("Ingrese una opción"))
 while(entrada != "ESC"){
     if (entrada === 1) {
-        const dietaEnergetica = new Dieta(semana,comidaEnergetica);
-        console.log(dietaEnergetica);
-        console.log(dietaEnergetica.asignarEnergia());
+        const dietaCansados = new Fusion(semana,comidaEnergetica);
+         dietaEnergia = [(dietaCansados.asignarEnergia())];
+         console.log(dietaEnergia);
+         alert("Ha seleccionado una dieta de alimentos rica en propiedades energeticas. Ahora debera seleccionar cualquier dia de la semana para conocer que incluir en su alimentación")
+                 dietaEnergia.forEach(iterar => {
+                  let elegirDia = prompt("Ingrese un dia de la semana para conocer que alimento incluir en su comida");
+                 while (elegirDia != "ESC") {
+                  switch (elegirDia) {
+                    case "lunes":
+                        alert(iterar.Lunes);
+                        break;
+            
+                        case "martes":
+                        alert(iterar.Martes);
+                        break;
+            
+                        case "miercoles":
+                        alert(iterar.Miercoles);
+                        break;
+            
+                        case "jueves":
+                        alert(iterar.Jueves);
+                        break;
+            
+                        case "viernes":
+                        alert(iterar.Viernes);
+                        break;
+            
+                        case "sabado":
+                        alert(iterar.Sabado);
+                        break;
+            
+                        case "domingo":
+                        alert(iterar.Domingo);
+                        break;
+
+                        case "0":
+                        opciones()
+                        break;
+
+                     default: ""
+                     alert("Ingrese un dia de la semana")
+                        break;
+                  }
+                  elegirDia = prompt("Ingrese un dia de la semana para conocer que alimento incluir en su comida" + "\n" + "\n" + "Si desea volver al menu de opciones ingrese 0");
+                }      
+            });
         break;
     }else if (entrada === 2) {
-        const dietaHierro = new Dieta(semana,comidaHierro);
+        const dietaAnemicos = new Fusion(semana,comidaHierro);
+        dietaHierro = [(dietaAnemicos.asignarHierro())];
         console.log(dietaHierro);
-        console.log(dietaHierro.asignarHierro());
+        alert("Ha seleccionado una dieta de alimentos rica en hierro. Ahora debera seleccionar cualquier dia de la semana para conocer que incluir en su alimentación")
+                dietaHierro.forEach(iterar => {
+                 let elegirDia = prompt("Ingrese un dia de la semana para conocer que alimento incluir en su comida");
+                while (elegirDia != "ESC") {
+                 switch (elegirDia) {
+                   case "lunes":
+                       alert(iterar.Lunes);
+                       break;
+           
+                       case "martes":
+                       alert(iterar.Martes);
+                       break;
+           
+                       case "miercoles":
+                       alert(iterar.Miercoles);
+                       break;
+           
+                       case "jueves":
+                       alert(iterar.Jueves);
+                       break;
+           
+                       case "viernes":
+                       alert(iterar.Viernes);
+                       break;
+           
+                       case "sabado":
+                       alert(iterar.Sabado);
+                       break;
+           
+                       case "domingo":
+                       alert(iterar.Domingo);
+                       break;
+                 
+                       case "0":
+                        opciones()
+                        break;
+
+                     default: ""
+                     alert("Ingrese un dia de la semana")
+                        break;
+                  }
+                  elegirDia = prompt("Ingrese un dia de la semana para conocer que alimento incluir en su comida" + "\n" + "\n" + "Si desea volver al menu de opciones ingrese 0");
+               }      
+           });
         break;
     }else if (entrada === 3) {
-        const dietaProteica = new Dieta(semana,comidaProteica);
+        const dietaDebiles = new Fusion(semana,comidaProteica);
+        dietaProteica = [(dietaDebiles.asignarProteina())];
         console.log(dietaProteica);
-        console.log(dietaProteica.asignarProteina());
+        alert("Ha seleccionado una dieta de alimentos rica en proteinas. Ahora debera seleccionar cualquier dia de la semana para conocer que incluir en su alimentación")
+                dietaProteica.forEach(iterar => {
+                 let elegirDia = prompt("Ingrese un dia de la semana para conocer que alimento incluir en su comida");
+                while (elegirDia != "ESC") {
+                 switch (elegirDia) {
+                   case "lunes":
+                       alert(iterar.Lunes);
+                       break;
+           
+                       case "martes":
+                       alert(iterar.Martes);
+                       break;
+           
+                       case "miercoles":
+                       alert(iterar.Miercoles);
+                       break;
+           
+                       case "jueves":
+                       alert(iterar.Jueves);
+                       break;
+           
+                       case "viernes":
+                       alert(iterar.Viernes);
+                       break;
+           
+                       case "sabado":
+                       alert(iterar.Sabado);
+                       break;
+           
+                       case "domingo":
+                       alert(iterar.Domingo);
+                       break;
+                 
+                       case "0":
+                        opciones()
+                        break;
+
+                     default: ""
+                     alert("Ingrese un dia de la semana")
+                        break;
+                  }
+                  elegirDia = prompt("Ingrese un dia de la semana para conocer que alimento incluir en su comida" + "\n" + "\n" + "Si desea volver al menu de opciones ingrese 0");
+               }      
+           });
         break;
      }else {
          entrada = parseFloat(prompt("Ingrese una opción"));
         }
-}    
-   
-// const dieta = new Dieta(semana, entrada)
-// console.log(dieta);
-// console.log(dieta.asignar());
-    
-//     crear(){
-//     let creacion = parseFloat(prompt("Seleccione una opción"))
-//         if (creacion === 1) {
-//             for(let i = 0; i < semana.length; i++){
-//                 objeto[semana[i]] = comidaHierro[i];
-                
-//             }
-            
-//             return objeto
-        
-//     }
-//  }   
+}
+}  
 
-// dietaHierro.push(objeto);
-// console.log(dietaHierro);
-// for(let i = 0; i < semana.length; i++){
-    //     objeto[semana[i]] = comidaHierro[i];
-    // return objeto
-
-//     let entrada = prompt("Ingrese el dia para conocer que debe comer");
-//     if(entrada === "Lunes"){
-//         alert(dieta.Lunes);
-//     }else{
-        
-//     }
-
-
-
-
-
-
-
-
-//opciones()
+opciones();
